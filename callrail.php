4bc49e1b684d81a894ece2da9d896a700a594dfa
callrail <callrail@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-02 03:16:18 +0000

    Version 0.3 * Update to version 2 of the javascript tracking script (swap.js) * Prompt the user to add an API key after installation. * Don't insert the CallRail script if no API key is present. * Trim whitespace surrounding the API key before saving.
    git-svn-id: https://plugins.svn.wordpress.org/callrail-phone-call-tracking/trunk@513311 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- callrail.php
+++ callrail.php
@@ -4,34 +4,42 @@ Plugin Name: CallRail Phone Call Tracking
 Plugin URI: http://www.callrail.com/docs/wordpress
 Description: Dynamically swap CallRail tracking phone numbers based on the visitor's referring source.
 Author: Last Mile Metrics, LLC.
-Version: 0.2.1
+Version: 0.3
 Author URI: http://www.callrail.com
 */
 
 add_action('admin_menu', 'callrail_menu');
+add_action('admin_notices', 'callrail_admin_notice' );
 add_action('wp_footer', 'callrail_footer');
 
 function callrail_menu() {
 	add_options_page('CallRail Options', 'CallRail', 'manage_options', 'callrail', 'callrail_options');
 }
 
+function callrail_admin_notice() {
+	$api_key = trim(get_option('masked_id_and_access_key'));
+	$is_plugins_page = (substr( $_SERVER["PHP_SELF"], -11 ) == 'plugins.php') ? true : false;
+	if( $is_plugins_page && !$api_key && function_exists( "admin_url" ) )
+		echo '<div class="error"><p><strong>' . sprintf( __('<a href="%s">Enter your CallRail API key</a> to enable dynamic tracking number insertion.', 'callrail' ), admin_url( 'options-general.php?page=callrail' ) ) . '</strong></p></div>';
+}
+
+
 function callrail_options() {
 
-      //must check that the user has the required capability 
-      if (!current_user_can('manage_options'))
-      {
-        wp_die( __('You do not have sufficient permissions to access this page.') );
-      }
+	//must check that the user has the required capability 
+	if (!current_user_can('manage_options')) {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
 
-      // Read in existing option value from database
-      $masked_id_and_access_key = get_option('masked_id_and_access_key');
+	// Read in existing option value from database
+	$masked_id_and_access_key = get_option('masked_id_and_access_key');
       
 
       // See if the user has posted us some information
       // If they did, this hidden field will be set to 'Y'
       if( isset($_POST['callrail_hidden_field']) && $_POST['callrail_hidden_field'] == 'Y' ) {
           // Read their posted value
-          $masked_id_and_access_key = $_POST['masked_id_and_access_key'];
+          $masked_id_and_access_key = trim($_POST['masked_id_and_access_key']);
           // Change the delimiter from x to /
           // x allows double clicking to copy and paste
           $masked_id_and_access_key = str_replace('x', '/', $masked_id_and_access_key);
@@ -54,17 +62,17 @@ function callrail_options() {
   	<p>Dynamically swap CallRail phone numbers based on the referring source.</p>
   	<form method="POST" action="">
   	<input type="hidden" name="callrail_hidden_field" value="Y">
-  	<table class="form-table"> 
+  	<table class="form-table" cellpadding="0" cellspacing="0"> 
       <tr valign="top"> 
-        <th scope="row">
-          <label for="masked_id_and_access_key">CallRail Wordpress ID</label>
+        <th scope="row" style="padding-left: 0px">
+          <label for="masked_id_and_access_key">CallRail API Key</label>
         </th> 
         <td>
           <input name="masked_id_and_access_key" type="text" id="masked_id_and_access_key" class="regular-text code" size="20" value="<?php echo $masked_id_and_access_key ?>" /> 
         </td> 
       </tr> 
       <tr>
-        <td colspan='2'><span class="description">You can find this value in your <a href="http://app.callrail.com/wordpress">CallRail account</a>.</span></td>
+        <td colspan="2" style="padding-left: 0px"><span class="description">You can find this value in your <a href="http://app.callrail.com/wordpress" target="_blank">CallRail account</a>.</span></td>
       </tr> 
     </table>
     <p class="submit"> 
@@ -76,8 +84,9 @@ function callrail_options() {
 <?php
 }
 
-function callrail_footer(){
-?>
-<script type="text/javascript" src="//app.callrail.com/companies/<?php echo get_option('masked_id_and_access_key') ?>/1/swap.js"></script>
-<?php
+function callrail_footer() {
+	$api_key = get_option('masked_id_and_access_key');
+	if (trim($api_key)) {
+		echo "<script type=\"text/javascript\" src=\"//app.callrail.com/companies/{$api_key}/2/swap.js\"></script>";
+	}
 }
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === CallRail Phone Call Tracking ===
 Contributors: callrail
-Tags: call tracking, analytics, seo, ppc, google adwords, offline conversion tracking, marketing optimization
+Tags: call tracking, call metrics, call tracking, analytics, seo, ppc, google adwords, conversion tracking, offline conversions, marketing optimization
 Requires at least: 3.0
-Tested up to: 3.2.1
-Stable tag: 0.2.1
+Tested up to: 3.3.1
+Stable tag: 0.3
 
 Dynamically swap CallRail tracking phone numbers based on the visitor's referring source.
 
@@ -21,3 +21,10 @@ See the documentation at http://www.callrail.com/docs/wordpress/.
 = 0.2 =
 
 * Initial public release.
+
+= 0.3 =
+
+* Update to version 2 of the javascript tracking script (swap.js)
+* Prompt the user to add an API key after installation.
+* Don't insert the CallRail script if no API key is present.
+* Trim whitespace surrounding the API key before saving.
